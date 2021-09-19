a57002be1505f99a058eaca7b96be5403e93ff51
joon <joonr7@gmail.com>
2021-09-19 18:43:05 +0900

    Solve: exercise2-1

--- src/exercise2-1/exercise2-1.cc
+++ src/exercise2-1/exercise2-1.cc
@@ -4,40 +4,58 @@
 
 using namespace std;
 
-class polynomial{
+class Polynomial
+{
+  friend ostream& operator<<(ostream&, const Polynomial&);
+
 public:
-  polynomial(const int& n)
-    : order{n}, coefficient{new vector<double>(order)}
+  Polynomial(const int& n)
+    : order_(n), coefficient_(vector<double>(order_ + 1, 0)) {}
+
+  ~Polynomial() {}
+
+  void SetCoefficient()
   {
-    // vector<double> v(order);
-    // coefficient.resize(order);
-    cout << coefficient.size() << endl;
-    // for (auto it = 0; it < order; ++it)
-    //   coefficient(it) = 3.0;
-    
-    
+    for (auto it = 0; it < coefficient_.size(); ++it)
+    {
+      cout << "Coefficient of " << order_ - it << "th degree term ";
+      cin >> coefficient_[it];
+    }
   }
 
-  ~polynomial() 
+  void FileOut()
   {
-    delete[] coefficient;
-  }
-
-  void print_file()
-  {
-    string filename = "polynomial.txt";
-    ofstream txtfile;
-    txtfile.open(filename);
-    txtfile << coefficient << endl;
-    txtfile.close();
+    string filename = "Polynomial.txt";
+    ofstream txtfile(filename);
+    for (auto it = 0; it < coefficient_.size(); ++it)
+      txtfile << order_ - it << "th degree term: " << coefficient_[it] << endl;
     cout << "File[" << filename << "] is saved." << endl;
   }
 
 private:
-  int order;
-  vector<double> *coefficient;
+  int order_;
+  vector<double> coefficient_;
 };
+
+ostream &operator <<(ostream &o, const Polynomial &P)
+{
+  for (auto it = 0; it < P.coefficient_.size(); ++it)
+      o << P.order_ - it << "th degree term: " << P.coefficient_[it] << endl;
+  return o;
+}
+
+ostream &operator <<(ostream &o, const Polynomial *Pp)
+{
+  o << *Pp;
+  return o;
+}
+
 int main()
 {
+  Polynomial p5(3);
+  p5.SetCoefficient();
+  cout << p5 << endl;
+  // p5.FileOut();
 
+  return 0;
 }
